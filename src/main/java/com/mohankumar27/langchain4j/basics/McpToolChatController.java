8d8b0f8626429eaaf0c2ba3cb5aeda985b4407d0
Mohan Kumar Sagadevan <mohankumarsagadevan@Mohans-MacBook-Air.local>
2026-01-26 12:38:32 +0530

    remove qualifier

--- src/main/java/com/mohankumar27/langchain4j/basics/McpToolChatController.java
+++ src/main/java/com/mohankumar27/langchain4j/basics/McpToolChatController.java
@@ -4,7 +4,6 @@ import com.mohankumar27.langchain4j.service.SearchAssistant;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.tool.ToolProvider;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class McpToolChatController {
     private final SearchAssistant assistant;
 
-    public McpToolChatController(ChatModel chatModel, @Qualifier("tavilyToolProvider") ToolProvider tavilyToolProvider) {
+    public McpToolChatController(ChatModel chatModel, ToolProvider tavilyToolProvider) {
         this.assistant = AiServices.builder(SearchAssistant.class)
                 .chatModel(chatModel)
                 .toolProvider(tavilyToolProvider)
